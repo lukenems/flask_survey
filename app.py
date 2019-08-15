bbd0ef81da8e27f223af157277a75857bc9fc811
Luke Nemy <lukenemy7@gmail.com>
2019-08-15 10:48:58 -0700

    added html template

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "super_secret"
+
+debug = DebugToolbarExtension(app)
